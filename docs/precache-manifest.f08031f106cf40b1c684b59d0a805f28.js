36e402cd8d86986f47d011e9c2962bc7f6663353
Jorge Julián Acero Lomelí <julianacero98@gmail.com>
2020-09-29 17:03:19 -0500

    gh-pages

---
+++ docs/precache-manifest.f08031f106cf40b1c684b59d0a805f28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc9ec60de41e13ee5b1ec31083b17afb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb4a7768d9964b922cbf",
+    "url": "/static/css/main.149b47bf.chunk.css"
+  },
+  {
+    "revision": "82f65f15faf6f200f227",
+    "url": "/static/js/2.c91139d0.chunk.js"
+  },
+  {
+    "revision": "bb4a7768d9964b922cbf",
+    "url": "/static/js/main.84a1da26.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "aa8e380cec1e7e15e5e223a423ce0e04",
+    "url": "/static/media/phone.aa8e380c.png"
+  },
+  {
+    "revision": "e723483dd14afb24ca9581ae675d415d",
+    "url": "/static/media/rhinowaresm.e723483d.png"
+  }
+]);
